ec6db52e820998dfb2545f6420b466bca8a67b0b
dejavu284 <skuratov.1@mail.ru>
2022-10-11 21:31:24 +0700

    First methods

--- Tetris/Form1.Designer.cs
+++ Tetris/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Tetris
 {
-    partial class Form1
+    partial class Tetris
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -30,12 +30,32 @@ namespace Tetris
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Tetris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.DoubleBuffered = true;
+            this.Name = "Tetris";
+            this.Text = "Tetris";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Tetris_Paint);
+            this.Resize += new System.EventHandler(this.Tetris_Resize);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Tetris/Form1.cs
+++ Tetris/Form1.cs
@@ -10,11 +10,100 @@ using System.Windows.Forms;
 
 namespace Tetris
 {
-    public partial class Form1 : Form
+    public partial class Tetris : Form
     {
-        public Form1()
+        public void Window(){
+            int[,] matrix = new int[10,20];
+        }
+        public void LFigure() {
+            int[,] figure =
+            {
+                {1,0,0 },
+                {1,0,0 },
+                {1,1,0 }
+            };
+        }
+        public void JFigure(){
+            int[,] figure =
+            {
+                {0,0,1 },
+                {0,0,1 },
+                {0,1,1 }
+            };
+        }
+        public void LineFigure(){
+            int[,] figure =
+            {
+                {1,0,0,0 },
+                {1,0,0,0 },
+                {1,0,0,0 },
+                {1,0,0,0 }
+            };
+        }
+        public void SquearFigure(){
+            int[,] figure =
+            {
+                {1,1 },
+                {1,1 }
+            };
+        }
+        public void TFigure(){
+            int[,] figure =
+            {
+                {1,0,0 },
+                {1,1,0 },
+                {1,0,0 }
+            };
+        }
+        public void SFigure(){ 
+            int[,] figure =
+            {
+                {0,0,0 },
+                {0,1,1 },
+                {1,1,0 }
+            };
+        }
+        public void ZFigure(){
+            int[,] figure =
+            {
+                {0,0,0 },
+                {1,1,0 },
+                {0,1,1 }
+            };
+        }
+        public void MatrixTransponision2X2(int [,] matrix) {
+            for (int i = 0; i < matrix.GetLength(0); i++){
+                for (int j = 0; j < i; j++){
+                    (matrix[i,j], matrix[j,i]) = (matrix[j,i],matrix[i, j]);
+                }
+            }
+        }
+        public void MatrixTransponision3X3(int[,] matrix){
+            for (int i = 0; i < matrix.GetLength(0); i++){
+                for (int j = 0; j < i; j++){
+                    (matrix[i, j], matrix[j, i]) = (matrix[j, i], matrix[i, j]);
+                }
+            }
+        }
+        public Tetris()
         {
             InitializeComponent();
+            Size = new Size(300, 600);
+        }
+
+        private void Tetris_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Tetris_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
